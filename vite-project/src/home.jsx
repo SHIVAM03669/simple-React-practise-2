0c73f109f22a0600f518e317f5e6545309cddca1
Shivam <shivam.s72@kalvium.community>
2025-03-05 16:34:36 +0530

    first commit

---
+++ vite-project/src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Shivam singh</h1>
+      <p>Email: Shivam@gmail.com</p>
+      <p>Phone: +91 7424961XXX</p>
+    </div>
+  );
+}
+
+export default Home;
